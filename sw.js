fcc6798670ec062f641347ff88b839d66f7db79b
Paulo Magro Casmal <99355790+pcasmal@users.noreply.github.com>
2026-09-02 21:10:56 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // Estratégia: network-first com fallback para cache. Assim o app funciona
 // offline (útil na quadra) mas sempre pega a versão mais nova quando há internet.
 // Ao publicar uma atualização, troque o número da versão abaixo (ex.: v2, v3...).
-const CACHE = 'futdajuf-v30';
+const CACHE = 'futdajuf-v31';
 const ASSETS = [
   './',
   './index.html',
